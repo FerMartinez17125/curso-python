78b7eae8596867a7082071a641ebbc001a61b42e
FerMartinez17125 <fermtzglez1@gmail.com>
2026-08-02 15:58:04 -0600

    Actualización de archivo STR_Methods

--- INTRODUCCION/STR_Methods.py
+++ INTRODUCCION/STR_Methods.py
@@ -6,3 +6,23 @@ text1_lower = text1.lower()
 print("Texto original: ", text1)
 print("Texto en mayúsculas: ", text1_upper)
 print("Texto en minúsculas: ", text1_lower)
+
+#Uso de find e index
+print()
+text = 'Hola Fernando, como estas?'
+print(text.find('Fernando'))
+print(text.index('Fernando'))
+#La diferencia entre find e index es que find devuelve -1 si no encuentra el texto, mientras que index lanza un error.
+print()
+#Startwith y endswith
+print(text.startswith('Hola'))
+print(text.endswith('estas?'))
+
+print()
+number = '1234'
+decimal = '1234.56'
+mix = 'Python3'
+
+print(number.isnumeric())
+print(decimal.isdigit())
+print(mix.isalnum())
