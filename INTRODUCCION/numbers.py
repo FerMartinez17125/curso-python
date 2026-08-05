ab93d0c5b0032c2e24815e1000bfd58a2640ebda
FerMartinez17125 <fermtzglez1@gmail.com>
2026-08-04 21:42:10 -0600

    Se agrega archivo de tipos de valores numericos

---
+++ INTRODUCCION/numbers.py
@@ -0,0 +1,12 @@
+
+age = 22
+big_int = 1234567890123456789012345678901234567890
+decimal = 3.1415
+
+print(age)
+print(type(age))
+
+print(big_int)
+print(type(big_int))
+print(decimal)
+print(type(decimal))
